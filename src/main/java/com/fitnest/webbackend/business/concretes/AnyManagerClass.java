fe8d2ca25d3d7d0938fa7492bf025adbec77ef35
IamKarimovich <karimovruslan043@gmail.com>
2025-11-19 12:07:08 +0400

    first init

---
+++ src/main/java/com/fitnest/webbackend/business/concretes/AnyManagerClass.java
@@ -0,0 +1,6 @@
+package com.fitnest.webbackend.business.concretes;
+
+import com.fitnest.webbackend.business.abstracts.AnyServiceInterface;
+
+public class AnyManagerClass implements AnyServiceInterface {
+}
