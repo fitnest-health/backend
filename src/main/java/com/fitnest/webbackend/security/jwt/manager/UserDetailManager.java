a27c8ee898e0b434a5d9661d804c45fde6629c3a
IamKarimovich <karimovruslan043@gmail.com>
2025-11-19 12:08:07 +0400

    first init

---
+++ src/main/java/com/fitnest/webbackend/security/jwt/manager/UserDetailManager.java
@@ -0,0 +1,8 @@
+package com.fitnest.webbackend.security.jwt.manager;
+
+/*
+    This class is for creating custom UserDetails class by implementing UserDetails
+ */
+
+public class UserDetailManager {
+}
