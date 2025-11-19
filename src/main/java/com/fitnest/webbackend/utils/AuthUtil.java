2d82bb4449c6ad64a814fd6dfb1f7b48566e67df
IamKarimovich <karimovruslan043@gmail.com>
2025-11-19 12:07:26 +0400

    first init

---
+++ src/main/java/com/fitnest/webbackend/utils/AuthUtil.java
@@ -0,0 +1,11 @@
+package com.fitnest.webbackend.utils;
+
+import org.springframework.stereotype.Component;
+
+/*
+    this class is for getting current user and his information in detail
+ */
+
+@Component
+public class AuthUtil {
+}
