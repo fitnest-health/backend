7e5ad492b89d194f0af9807503adf40da0dedc2a
IamKarimovich <karimovruslan043@gmail.com>
2025-11-19 12:07:21 +0400

    first init

---
+++ src/main/java/com/fitnest/webbackend/exceptions/AppException.java
@@ -0,0 +1,25 @@
+package com.fitnest.webbackend.exceptions;
+
+
+public abstract class AppException extends RuntimeException {
+  private final String errorCode;
+
+  public AppException(String message, String errorCode) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+
+  public String getErrorCode() {
+    return errorCode;
+  }
+}
+
+
+
+
+
+
+
+
+
+
