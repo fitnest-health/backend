d386ad0618f847fde33ae7eb971fe55cdb54bc0c
IamKarimovich <karimovruslan043@gmail.com>
2025-11-27 20:35:54 +0400

    Entitiler yaradildi

---
+++ src/main/java/com/fitnest/webbackend/model/enums/QuestionType.java
@@ -0,0 +1,8 @@
+package com.fitnest.webbackend.model.enums;
+
+public enum QuestionType {
+    SINGLE_SELECT, // Radio buttons
+    MULTI_SELECT,  // Checkboxes
+    INPUT_NUMBER,  // Weight, Height
+    INPUT_TEXT     // Name, etc.
+}
